d933fdef265d5dfaf4298bfdd796b143d7642b93
yuta_sh <yuta_sh@LAPTOP-G7439E22.localdomain>
2022-01-03 02:35:42 +0900

    [feat]test5追加

---
+++ select10/test5.js
@@ -0,0 +1,44 @@
+function Main(input) {
+  input = input.trim().split("\n")[0].split(" ");
+
+  //文字列から10進数に変換するときはparseIntを使う
+  const N = input[0];
+  const A = input[1];
+  const B = input[2];
+
+  const sum = (...args) => {
+    return args.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+  };
+
+  const makeResult = (Ni) => {
+    let arrayNi = [];
+    const NiString = String(Ni);
+    //   各桁の和を求める
+    for (let index = 0; index < NiString.length; index++) {
+      const NiNum = parseInt(NiString.slice(index, index + 1), 10);
+      arrayNi.push(NiNum);
+    }
+
+    // A~Bまでのものであった場合にresultに入れる
+    const sumNi = sum(...arrayNi);
+    if (sumNi >= A && sumNi <= B) {
+      result.push(parseInt(Ni, 10));
+    }
+  };
+
+  let result = [];
+
+  for (let index = 0; index <= parseInt(N, 10); index++) {
+    makeResult(index);
+  }
+
+  //出力
+  console.log("%d", sum(...result));
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+Main(require("fs").readFileSync("./input.txt", "utf8"));
+// 本番は下記で提出
+// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
