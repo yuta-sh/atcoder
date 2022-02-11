285916ec58fcc9a0d0c5f63164fe067b8f205ad5
yuta_sh <yuta.dev79@gmail.com>
2022-02-12 01:58:43 +0900

    [mod]テンプレの書き方を見やすいように変更

--- all/235/b.js
+++ all/235/b.js
@@ -1,22 +1,32 @@
-function Main(input) {
-  input = input.trim().split("\n");
-  const n = input[0];
-  const H = input[1].split(" ").map((str) => Number(str));
+// ローカルは下記で実行
+// -------------------------------------------------------------
+// const input = require("fs")
+//   .readFileSync("./input.txt", "utf8")
+//   .trim()
+//   .split("\n");
 
-  let now = H[0];
-  for (let i = 1; i < n; i++) {
-    if (now >= H[i]) {
-      break;
-    }
-    now = H[i];
+// 本番は下記で提出
+// -------------------------------------------------------------
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n");
+
+// -------------------------------------------------------------
+
+const n = input[0];
+const H = input[1].split(" ").map((str) => Number(str));
+
+let now = H[0];
+for (let i = 1; i < n; i++) {
+  if (now >= H[i]) {
+    break;
   }
-  console.log(now);
+  now = H[i];
 }
+console.log(now);
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-// Main(require("fs").readFileSync("./input.txt", "utf8"));
-// 本番は下記で提出
-Main(require("fs").readFileSync("/dev/stdin", "utf8"));
 
 // かかった時間:8:18
 // なんか今回超絶簡単だったな。
--- template/b.js
+++ template/b.js
@@ -1,17 +1,19 @@
-function Main(input) {
-  input = input.trim().split("\n");
+// ローカルは下記で実行
+// -------------------------------------------------------------
+const input = require("fs")
+  .readFileSync("./input.txt", "utf8")
+  .trim()
+  .split("\n");
 
-  //文字列から10進数に変換するときはparseIntを使う
-  const a = parseInt(input[0], 10);
-  const s = "test";
-
-  //出力
-  console.log("%d %s", a, s);
-}
-
-//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-Main(require("fs").readFileSync("./input.txt", "utf8"));
 // 本番は下記で提出
-// Main(require("fs").readFileSync("/dev/stdin", "utf8"));
+// -------------------------------------------------------------
+// const input = require("fs")
+//   .readFileSync("/dev/stdin", "utf8")
+//   .trim()
+//   .split("\n");
 
-// かかった時間:
+// -------------------------------------------------------------
+
+// [memo]かかった時間:
+
+// 以降処理記載
