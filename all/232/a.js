bafaff7f1b79f55447262307e9b6819f7baa5153
yuta_sh <yuta.dev79@gmail.com>
2022-02-12 21:00:56 +0900

    [add]232

---
+++ all/232/a.js
@@ -0,0 +1,21 @@
+// ローカルは下記で実行
+// -------------------------------------------------------------
+// const input = require("fs")
+//   .readFileSync("./input.txt", "utf8")
+//   .trim()
+//   .split("\n");
+
+// 本番は下記で提出
+// -------------------------------------------------------------
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n");
+
+// -------------------------------------------------------------
+
+// [memo]かかった時間:2:37
+
+// 以降処理記載
+const result = input[0].split("x").map((str) => Number(str));
+console.log(result[0] * result[1]);
---
+++ all/232/b.js
@@ -0,0 +1,53 @@
+// ローカルは下記で実行
+// -------------------------------------------------------------
+const input = require("fs")
+  .readFileSync("./input.txt", "utf8")
+  .trim()
+  .split("\n");
+
+// 本番は下記で提出
+// -------------------------------------------------------------
+// const input = require("fs")
+//   .readFileSync("/dev/stdin", "utf8")
+//   .trim()
+//   .split("\n");
+
+// -------------------------------------------------------------
+
+// [memo]かかった時間:46:21
+// 調べるのに12分
+// 全部で迷った。小分割対象のnextの出し方、ループを回すときに前回の文字列から更新していないこと、最後のループ脱出方法
+
+// 小分割対象のnextの出し方→これはレアケースはおいておいて、まずは他の代表ケースから考えるべきだった。
+// 最後のループ脱出→このファイル自体がくくっていなくても大きな関数扱いなのでreturnでおｋ。
+
+// 以降処理記載
+
+const S = input[0].split("");
+const Tstr = input[1];
+
+const baseNum = "a".charCodeAt(0);
+const lastNum = "z".charCodeAt(0);
+
+const nextChar = (str) => {
+  return String.fromCharCode(
+    ((str.charCodeAt(0) - baseNum + 1) % (lastNum - baseNum + 1)) + baseNum
+  );
+};
+
+let isYes = false;
+let nextS = [...S];
+for (let i = 0; i < lastNum - baseNum + 1; i++) {
+  nextS = [...nextS.map((str) => nextChar(str))];
+
+  // S.map((str) => nextChar(String.fromCharCode(str.charCodeAt(0) + i)));
+  if (nextS.join("") === Tstr) {
+    console.log("Yes");
+    // isYes = true;
+    return;
+  }
+}
+
+// if (!isYes) {
+console.log("No");
+// }
