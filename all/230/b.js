c197dc30453d894521014302a129503ebc5c514d
yuta_sh <yuta.dev79@gmail.com>
2022-02-19 02:32:27 +0900

    [add]230 b

---
+++ all/230/b.js
@@ -0,0 +1,31 @@
+// ローカルは下記で実行
+// -------------------------------------------------------------
+// const input = require("fs")
+//   .readFileSync("./input.txt", "utf8")
+//   .trim()
+//   .split("\n");
+
+// 本番は下記で提出
+// -------------------------------------------------------------
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n");
+
+// -------------------------------------------------------------
+
+// [memo]かかった時間:9:01
+
+// 以降処理記載
+const oxx = "oxx";
+const t = oxx.repeat(Math.pow(10, 5));
+
+const s = input[0];
+
+for (let i = 0; i < t.length; i++) {
+  if (t.slice(i, i + s.length) == s) {
+    console.log("Yes");
+    return;
+  }
+}
+console.log("No");
