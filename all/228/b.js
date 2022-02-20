22dff39e8bb3ab64868d1332c701874b145ea0f1
yuta_sh <yuta.dev79@gmail.com>
2022-02-20 19:19:21 +0900

    [fix]228 b

--- all/228/b.js
+++ all/228/b.js
@@ -14,7 +14,14 @@ const input = require("fs")
 
 // -------------------------------------------------------------
 
-// [memo]かかった時間:
+// [memo]かかった時間:42:21
+// TLEしてしまってかなり時間かかってしまった。
+// setを使ったらいけたので、TLEしているときの可能性の一つとして、重複を許さない値の場合に配列で検索しつつやってるとアルゴリズム的に大量の時間を食ってしまうのかもしれない
+// この問題はでも典型的なSet(ハッシュテーブル)を使う問題だな。
+// 重複を許さずに登録していくようなもので、気にしているのが値を保持しているのかどうかだけで、順番を気にしていない。まさにハッシュテーブルの要件。
+// ハッシュテーブルと同様の理由により、バケツ法でも回答可能。
+// きっと愚直にやったらTLEするようにできているんだろうこの問題は。
+// あ、愚直にやって一個前のコミット、outputFriredListが1,2,3,4,...Nになっていくから、計算量が1/2(N*(N+1))だ。つまりO(N^2)。で、Nは10^5まで。ということは10^10がmax。だからか。
 
 // 以降処理記載
 const n = input[0].split(" ").map((str) => Number(str))[0];
@@ -22,17 +29,20 @@ const x = input[0].split(" ").map((str) => Number(str))[1];
 
 const aList = input[1].split(" ").map((str) => Number(str));
 
-let outputFriendList = [x];
+let outputFriendList = new Set();
+outputFriendList.add(x);
+
 let inputFriend = x;
 let fetched = undefined;
 
 do {
   fetched = aList[inputFriend - 1];
 
-  if (!outputFriendList.some((num) => num === fetched)) {
-    outputFriendList.push(fetched);
+  if (!outputFriendList.has(fetched)) {
+    outputFriendList.add(fetched);
   }
-  inputFriend = fetched;
-} while (!outputFriendList.some((num) => num === aList[inputFriend - 1]));
 
-console.log(outputFriendList.length);
+  inputFriend = fetched;
+} while (!outputFriendList.has(aList[inputFriend - 1]));
+
+console.log(outputFriendList.size);
