c2af4800aadc57fce0ae8dee178fa5e6bca2d0ca
yuta_sh <yuta.dev79@gmail.com>
2022-02-23 13:33:42 +0900

    [mod]226 a done by string method

--- all/226/a.js
+++ all/226/a.js
@@ -18,6 +18,15 @@ const input = require("fs")
 // Math.roundを知らなかったから調べただけ。
 // jsのroundは負の数であっても小数部が0.5だったときに正の方向に大きい数に繰り上がるらしく、これが他の言語とは違うらしい。
 // 一応文字列処理でも実装しておくか。
+// 文字列処理の場合はindexOfを使うことである文字の位置を取得できる
+// 配列処理の場合もindexOfがある。
 
 // 以降処理記載
-console.log(Math.round(Number(input[0])));
+const dotPosition = input[0].indexOf(".");
+let intVal = Number(input[0].slice(0, dotPosition));
+
+if (Number(input[0].slice(-3, -2)) >= 5) {
+  intVal++;
+}
+
+console.log(intVal);
