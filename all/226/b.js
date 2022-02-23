590057591da98d4c6490ef0c73354a3b92bf304c
yuta_sh <yuta.dev79@gmail.com>
2022-02-23 14:00:49 +0900

    [add]226 b

--- all/226/b.js
+++ all/226/b.js
@@ -1,19 +1,31 @@
 // ローカルは下記で実行
 // -------------------------------------------------------------
-const input = require("fs")
-  .readFileSync("./input.txt", "utf8")
-  .trim()
-  .split("\n");
-
-// 本番は下記で提出
-// -------------------------------------------------------------
 // const input = require("fs")
-//   .readFileSync("/dev/stdin", "utf8")
+//   .readFileSync("./input.txt", "utf8")
 //   .trim()
 //   .split("\n");
 
+// 本番は下記で提出
+// -------------------------------------------------------------
+const input = require("fs")
+  .readFileSync("/dev/stdin", "utf8")
+  .trim()
+  .split("\n");
+
 // -------------------------------------------------------------
 
-// [memo]かかった時間:
+// [memo]かかった時間:11:57
+// 最近b問題でハッシュマップの利用が多いなあ。これには慣れてきた
 
 // 以降処理記載
+const n = Number(input[0]);
+
+const set = new Set();
+
+for (let i = 1; i < n + 1; i++) {
+  const iStr = input[i].slice(2);
+  if (!set.has(iStr)) {
+    set.add(iStr);
+  }
+}
+console.log(set.size);
